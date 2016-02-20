25b1f823969e55b5d42345d361f8a9fabb44a096
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-02-20 03:55:02 +0000

    Testing

--- zip_current_vm_into_bundle.bash
+++ zip_current_vm_into_bundle.bash
@@ -1,4 +1,4 @@
-function zip_up_the_vm() {
+function zip_up_the_vm_into_temp() {
     
     cd "$GOPATH"
     
@@ -16,21 +16,61 @@ function zip_up_the_vm() {
     zip -r "$_PATH_AND_FILE_TO_WRITE" . -x "*.c9*" "*c9_vm_zips*"
 }
 
+function unzip_bundle_into_temp() {
+    unzip "$_ZIPED_MACHINES_BUNDLE_PATH" -d "$_TEMP_DIR_PATH"
+}
+
+function zip_up_temp_into_bundle() {
+    
+    echo "_TEMP_DIR_PATH = $_TEMP_DIR_PATH";
+    read -p "zip bundle ? " _pause
+    zip -rj "$_TEMP_DIR_PATH/temp_bundle.zip" "$_TEMP_DIR_PATH/."
+}
+
+function move_temp_bundle_to_zipped_machine_folder() {
+    if [ -e "$_ZIPED_MACHINES_BUNDLE_PATH" ]
+    then
+        mv "$_ZIPED_MACHINES_BUNDLE_PATH" "$_ZIPED_MACHINES_BUNDLE_PATH-archived"
+    fi
+    
+    mv "$_TEMP_DIR_PATH/temp_bundle.zip" "$_ZIPED_MACHINES_BUNDLE_PATH";
+    #rename "$_ZIPED_MACHINES_FOLDER_PATH/temp_bundle.zip" "$_ZIPED_MACHINES_BUNDLE_PATH"
+}
+
 function init() {
     local _ZIP_FILE_MODIFIER=1;
     local _ZIP_FILE_NAME="$C9_PROJECT";
-    local _ZIPED_MACHINES_FOLDER_PATH="$PWD"
     local _TEMP_DIR_NAME="TEMP";
     local _TEMP_DIR_PATH="$PWD/$_TEMP_DIR_NAME";
     local _ZIPED_MACHINES_FOLDER_NAME="zipped_machines";
+    local _ZIPED_MACHINES_FOLDER_PATH="$PWD/$_ZIPED_MACHINES_FOLDER_NAME";
     local _ZIP_FILE_EXTENSION="zip";
     
     local _PATH_AND_FILE_TO_WRITE="$_TEMP_DIR_PATH/$_ZIP_FILE_NAME.$_ZIP_FILE_EXTENSION";
     
-    local _ZIPED_MACHINES_BUNDLE_FILE_NAME="zipped_machines_bundle";
+    local _ZIPED_MACHINES_BUNDLE_NAME="zipped_machines_bundle";
+    local _ZIPED_MACHINES_BUNDLE_PATH="$_ZIPED_MACHINES_FOLDER_PATH/$_ZIPED_MACHINES_BUNDLE_NAME.$_ZIP_FILE_EXTENSION";
     
-    mkdir "$_TEMP_DIR_PATH"
-    zip_up_the_vm;
+    if [ ! -d "$_TEMP_DIR_PATH" ]
+    then
+        mkdir "$_TEMP_DIR_PATH"
+    fi
+    
+    if [ ! -d "$_ZIPED_MACHINES_FOLDER_PATH" ]
+    then
+        mkdir "$_ZIPED_MACHINES_FOLDER_PATH";
+    fi
+    
+    zip_up_the_vm_into_temp;
+    
+    if [ -e "$_ZIPED_MACHINES_BUNDLE_PATH" ]
+    then
+        unzip_bundle_into_temp;
+        rename "$_ZIPED_MACHINES_BUNDLE_PATH" "$_ZIPED_MACHINES_BUNDLE_PATH-archived"
+    fi
+    
+    zip_up_temp_into_bundle;
+    move_temp_bundle_to_zipped_machine_folder;
     #add_vm_to_bundle;
 }
 
